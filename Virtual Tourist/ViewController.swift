42dcfb3756e91c09488a71a5946a31fad627bd20
Luis Angel Vazquez Alor <36934792+LuisAlor@users.noreply.github.com>
2020-06-25 17:43:29 +0300

    Initial Commit

---
+++ Virtual Tourist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Virtual Tourist
+//
+//  Created by Luis Angel Vazquez Alor on 25.06.2020.
+//  Copyright © 2020 Luis Angel Vazquez Alor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
